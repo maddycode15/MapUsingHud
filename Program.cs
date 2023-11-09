39ef8a4d5f5e192eec9bb2e7323a1da460212f24
matthew allison restin <144450180+maddycode15@users.noreply.github.com>
2023-11-08 22:46:20 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// alright, this is it
+namespace MapUsingHud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("we are so back");
+            Console.ReadKey(true);
+
+
+
+
+
+
+
+            Console.ReadKey(true);
+            Console.WriteLine("press any button to continue");
+            Console.ReadKey(true);
+
+        }
+    }
+}
+
+
